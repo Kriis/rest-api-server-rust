35e408595444058cee10a7a7aa07a64e8dd14dcf
unknown <haianhkmt@gmail.com>
2023-04-19 10:18:39 +0700

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,77 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+
+use rocket_contrib::json::{Json, JsonValue};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize, Clone)] // add Clone trait here
+struct Book {
+    id: u32,
+    title: String,
+    author: String,
+    description: String,
+}
+
+
+// Define a static variable to hold all the books
+static mut BOOKS: Vec<Book> = Vec::new();
+
+#[get("/books")]
+fn get_all_books() -> Json<Vec<Book>> {
+    // Use an unsafe block to access the global variable
+    unsafe {
+        Json(BOOKS.clone())
+    }
+}
+
+#[get("/books/<id>")]
+fn get_book(id: u32) -> Option<Json<Book>> {
+    // Use an unsafe block to access the global variable
+    unsafe {
+        BOOKS.iter()
+        .find(|book| book.id == id)
+        .map(|book| Json(book.clone()))
+    }
+}
+
+#[post("/books", format = "json", data = "<book>")]
+fn create_book(book: Json<Book>) -> JsonValue {
+    let new_book = book.into_inner();
+
+    // Use an unsafe block to access the global variable
+    unsafe {
+        BOOKS.push(new_book.clone());
+    }
+
+    json!({
+        "status": "success",
+        "message": "Book created successfully",
+        "data": new_book
+    })
+}
+
+fn main() {
+    // Initialize the global variable with some books
+    unsafe {
+        BOOKS = vec![
+            Book {
+                id: 1,
+                title: "The Lord of the Rings".to_string(),
+                author: "J.R.R. Tolkien".to_string(),
+                description: "The Lord of the Rings is an epic high fantasy novel by the English author and scholar J. R. R. Tolkien.".to_string(),
+            },
+            Book {
+                id: 2,
+                title: "The Hobbit".to_string(),
+                author: "J.R.R. Tolkien".to_string(),
+                description: "The Hobbit, or There and Back Again is a children's fantasy novel by English author J. R. R. Tolkien.".to_string(),
+            }
+        ];
+    }
+
+    rocket::ignite()
+        .mount("/", routes![get_all_books, get_book, create_book])
+        .launch();
+}
